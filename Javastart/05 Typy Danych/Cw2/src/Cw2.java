e81d18782130c55a0f8c48a1247e570f9b297df0
00 <155826373+pistyczny@users.noreply.github.com>
2024-01-12 14:43:23 +0100

    Cwiczenie 2gie

--- Javastart/05 Typy Danych/Cw2/src/Cw2.java
+++ Javastart/05 Typy Danych/Cw2/src/Cw2.java
@@ -1,3 +1,34 @@
+//
+//Ćwiczenie
+//
+//        Poniżej znajduje się kod programu, który z jakiegoś powodu nie chce się skompilować.
+//class BasicTypes {
+//    public static void main(String[] args) {
+//        System.out.println('Mam Audi A4');
+//        System.out.println('Rocznik:');
+//        System.out.println(2004);
+//        System.out.println('Silnik o pojemności:');
+//        System.out.println(2,0);
+//    }
+//}
+//    Zidentyfikuj błędy, skompiluj i uruchom poniższy program.
+//
+//        Podpowiedź
+//        Zwróć uwagę na szczegóły i to w jaki sposób zapisywane są wartości typu String oraz double.
+public class Cw2 {
+    public static void main(String[] args) {
+        System.out.println("Mam Audi A4");
+        System.out.println("Rocznik:");
+        System.out.println(2004);
+        System.out.println("Silnik o pojemności:");
+        System.out.println(2.0);
+    }
+}
+
+
+
+
+
 public class Cw2 {
     public static void main(String[] args) {
         System.out.println("Mam Audi A4");
