f370526a3c5152051ec70012c35c4ee94d87e353
00 <155826373+pistyczny@users.noreply.github.com>
2024-01-12 14:37:11 +0100

    Cwiczenie 1

--- Javastart/05 Typy Danych/Cw1/src/Cw1.java
+++ Javastart/05 Typy Danych/Cw1/src/Cw1.java
@@ -2,7 +2,7 @@
     public static void main(String[] args) {
         System.out.println("Cześć Jan, słyszałem, że masz 25 lat i uczysz się programowania. Czy to prawda?");
         System.out.println(true);
-        System.out.println("czy twoje iniscjaly to J K?");
+        System.out.println("czy twoje iniscjaly to J K??");
         System.out.println("tak");
         System.out.println(true);
 
