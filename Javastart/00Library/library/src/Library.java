616963ec46b54bf814dbfe77960eb7eb87101b96
00 <155826373+pistyczny@users.noreply.github.com>
2024-01-12 15:11:28 +0100

    Cwiczenie pierwsze

--- Javastart/00Library/library/src/Library.java
+++ Javastart/00Library/library/src/Library.java
@@ -2,7 +2,7 @@
 //    public static void main(String[] args) {
 //        final String appName = "Biblioteka v0.1";
 //
-//        String title = "W pustyni i w puszczy";
+//        String title = "W pustyni i w puszczyyy";
 //        String author = "Henryk Sienkiewicz";
 //        int releaseDate = 2010;
 //        int pages = 296;
