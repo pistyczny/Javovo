fdbbab88702522af31e7d4feac0b79ca12c98f3a
00 <155826373+pistyczny@users.noreply.github.com>
2024-01-20 02:15:11 +0100

    Nocny commit, idk co za zmiany, dopiero siadlem ale niech se leci w swiat a co

--- Biblioteka/Biblioteka/src/Book.java
+++ Biblioteka/Biblioteka/src/Book.java
@@ -6,14 +6,17 @@ class Book {
     String publisher;
     String isbn;
 
-    Book(String bookTitle, String bookAuthor, int bookRelease,
-         int bookPages, String bookPublisher, String bookIsbn) {
-        title = bookTitle;
-        author = bookAuthor;
-        releaseDate = bookRelease;
-        pages = bookPages;
-        publisher = bookPublisher;
-        isbn = bookIsbn;
+    Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+       this(title,author,releaseDate, pages, publisher);
+       this.isbn = isbn;
+    }
+    Book(String bookTitle, String bookAuthor, int bookRelease, int bookPages, String bookPublisher, String bookIsbn) {
+        this.title = bookTitle;
+        this.author = bookAuthor;
+        this.releaseDate = bookRelease;
+        this.pages = bookPages;
+        this.publisher = bookPublisher;
+        this.isbn = bookIsbn;
     }
 
     void printInfo() {
